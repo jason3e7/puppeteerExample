272cbabc43e9ee9301f18778de15252010a9a5af
jason3e7 <jason3e7@gmail.com>
2020-01-22 16:59:44 +0800

    add gold

---
+++ gold.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const puppeteer = require('puppeteer');
+const chromeArgs = [
+	'--no-sandbox',
+	'--disable-setuid-sandbox',
+	'--disable-gpu',
+	'--hide-scrollbars',
+	'--mute-audio',
+	'--ignore-certificate-errors',
+	'--ignore-certificate-errors-spki-list',
+	'--ssl-version-max=tls1.3',
+	'--ssl-version-min=tls1',
+	'--disable-web-security',
+	'--allow-running-insecure-content',
+	'--proxy-bypass-list=<-loopback>',
+	'--window-size=1024,768'
+];
+
+(async () => {
+  // chromeArgs.push("--proxy-server=127.0.0.1:8080");
+  const browser = await puppeteer.launch({
+    // slowMo: 100,
+    // headless: false, 
+    // devtools: true, 
+    args: chromeArgs, ignoreHTTPSErrors:true,
+  });
+
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1024, height: 768 });
+
+  var cost = 1511;
+  var change = 0;
+  var diff = 0;
+
+  // gold
+  await page.goto('https://rate.bot.com.tw/gold');
+  var ask = await page.$eval('.footable-detail-show:nth-child(1) td:nth-child(3)', e => e.innerText);
+  var bid = await page.$eval('.footable-detail-show:nth-child(2) td:nth-child(3)', e => e.innerText);
+
+  // console.log('ask : ' + ask);
+  // console.log('bid : ' + bid);
+
+  var askNum = parseInt(ask.replace(/,/g, ""));
+  var bidNum = parseInt(bid.replace(/,/g, ""));
+
+  // console.log('diff : ' + diff);
+  // console.log('change : ' + ((bidNum - cost) * 100 / cost).toFixed(2) + "%");
+
+  await page.goto('https://www.kitco.com/charts/livegold.html');
+  var spBid = await page.$eval('#sp-bid', e => e.innerText);
+  var spAsk = await page.$eval('#sp-ask', e => e.innerText);
+
+  var message = "<br>"
+  message += "台銀賣出 : " + ask + "<br>"
+  message += "台銀買進 : " + bid + "<br>"
+  message += "價差 : " + (askNum - bidNum) + "<br>"
+  message += "報酬率 : " + ((bidNum - cost) * 100 / cost).toFixed(2) + "%<br>"
+  message += "=====<br>"
+  message += "國際賣出 : " + spAsk + "<br>"
+  message += "國際買進 : " + spBid + "<br>"
+
+  await page.setRequestInterception(true);
+  page.on('request', interceptedRequest => {
+    const headers = interceptedRequest.headers();
+    headers['Content-Type'] = 'application/json';
+
+    var data = {
+      'method': 'POST',
+      'postData': '{"value1":"' + message + '"}',
+      'headers': headers
+    };
+    interceptedRequest.continue(data);
+  });
+
+  await page.goto('https://maker.ifttt.com/trigger//with/key/');
+
+  await browser.close();
+})();
